1a031c42616c9a874efa2568a0a648de30e3c580
dinesh-bhagdev-csi <dinesh.bhagdev@cloudspikes.ca>
2025-10-07 19:00:32 -0400

    feat: add AWS provider configuration and remove redundant terraform block

--- infra/envs/test/outputs.tf
+++ infra/envs/test/outputs.tf
@@ -4,6 +4,6 @@ output "aws_sm_secret_name" {
 }
 
 output "aws_sm_secret_arn" {
-  description = "ARN of the AWS Secrets Manager secret"  
+  description = "ARN of the AWS Secrets Manager secret"
   value       = module.aws_sm_test.aws_sm_secret_arn
 }
