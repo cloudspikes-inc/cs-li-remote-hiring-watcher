9350d0b5a460cb66e7664afa6ded557b40b4f66a
dinesh-bhagdev-csi <dinesh.bhagdev@cloudspikes.ca>
2025-10-07 19:22:06 -0400

    feat: uncomment S3 backend configuration for Terraform in test environment

--- infra/envs/test/backend.tf
+++ infra/envs/test/backend.tf
@@ -1,10 +1,9 @@
-# Commented out for local testing - uncomment when ready for production
-# terraform {
-#   backend "s3" {
-#     bucket  = "cs-li-remote-hiring-watcher-terraform-state-test"
-#     key     = "test/terraform.tfstate"
-#     region  = "ap-south-1"
-#     profile = "default"
-#     encrypt = true
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket  = "cs-li-remote-hiring-watcher-terraform-state-test"
+    key     = "test/terraform.tfstate"
+    region  = "ap-south-1"
+    profile = "default"
+    encrypt = true
+  }
+}
